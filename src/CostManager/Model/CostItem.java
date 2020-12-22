fc063714e21902c622198bd7454730834402d008
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-22 14:59:16 +0200

    Add purchase date to ConstItem Class

--- src/CostManager/Model/CostItem.java
+++ src/CostManager/Model/CostItem.java
@@ -1,67 +1,76 @@
 package CostManager.Model;
 
+import java.sql.Date;
+
 public class CostItem {
 
     private int id;
+    private Date date;
     private Category category;
     private String description;
-    private double totalPrice;
     private Currency currency;
+    private double totalPrice;
 
     /**
      * CostItem Parameterized Constructor that builds CostItem object from Database.
      *
      @param id          Variable for holding the cost id from the database.
+     @param date        Represents the date of the purchase.
      @param category    Represents the category that the cost item belong to.
      @param description Represents any info about the cost item.
      @param totalPrice  The total price of the cost.
      @param currency    The currency that used during the purchase.
      *
      */
-    public CostItem(int id, Category category, String description, double totalPrice, Currency currency) {
+    public CostItem(int id, Date date, Category category, String description, Currency currency, double totalPrice) {
         this.id = id;
+        this.date = date;
         this.category = category;
         this.description = description;
-        this.totalPrice = totalPrice;
         this.currency = currency;
+        this.totalPrice = totalPrice;
     }
 
     /**
      * CostItem Parameterized Constructor that builds CostItem object without id.
      *
+     @param date        Represents the date of the purchase.
      @param category    Represents the category that the cost item belong to.
      @param description Represents any info about the cost item.
      @param totalPrice  The total price of the cost.
      @param currency    The currency that used during the purchase.
      *
      */
-    public CostItem(Category category, String description, double totalPrice, Currency currency) {
+    public CostItem(Date date, Category category, String description, Currency currency, double totalPrice) {
         this.id = -1;
+        this.date = date;
         this.category = category;
         this.description = description;
-        this.totalPrice = totalPrice;
         this.currency = currency;
+        this.totalPrice = totalPrice;
     }
 
     /**
      * CostItem Getters
      */
     public double getId() { return id; }
+    public Date getDate() { return date; }
     public Category getCategory() {
         return category;
     }
     public String getDescription() {
         return description;
     }
-    public double getTotalPrice() { return totalPrice; }
     public Currency getCurrency() { return currency; }
+    public double getTotalPrice() { return totalPrice; }
 
     /**
      * CostItem Setters
      * !!!!! need to add proper validation and move to the constructor
      */
+    public void setDate(Date date) { this.date = date; }
     public void setCategory(Category category) { this.category = category; }
     public void setDescription(String description) { this.description = description; }
-    public void setTotalPrice(double totalPrice) { this.totalPrice = totalPrice; }
     public void setCurrency(Currency currency) { this.currency = currency; }
+    public void setTotalPrice(double totalPrice) { this.totalPrice = totalPrice; }
 }
