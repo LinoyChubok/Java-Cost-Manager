ff47deb24f450bfb98cefc2c00cbd657d0beb242
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-22 13:12:43 +0200

    Update CostItem Constructors

--- src/CostManager/Model/CostItem.java
+++ src/CostManager/Model/CostItem.java
@@ -1,13 +1,6 @@
 package CostManager.Model;
 
 public class CostItem {
-<<<<<<< HEAD
-    private String description;
-    private double sum;
-    private Currency currency;
-    private int id;
-=======
->>>>>>> refs/remotes/origin/master
 
     private int id;
     private Category category;
@@ -16,7 +9,7 @@ public class CostItem {
     private Currency currency;
 
     /**
-     * CostItem Parameterized Constructor that builds CostItem object.
+     * CostItem Parameterized Constructor that builds CostItem object from Database.
      *
      @param id - Variable for holding the cost id from the database.
      @param category - Represents the category that the cost item belong to.
@@ -25,7 +18,24 @@ public class CostItem {
      @param currency - The currency that used during the purchase.
      *
      */
-    public CostItem(Category category, String description, double totalPrice, Currency currency, int id) {
+    public CostItem(int id, Category category, String description, double totalPrice, Currency currency) {
+        this.id = id;
+        this.category = category;
+        this.description = description;
+        this.totalPrice = totalPrice;
+        this.currency = currency;
+    }
+
+    /**
+     * CostItem Parameterized Constructor that builds CostItem object without id.
+     *
+     @param category - Represents the category that the cost item belong to.
+     @param description - Represents any info about the cost item.
+     @param totalPrice  - The total price of the cost.
+     @param currency - The currency that used during the purchase.
+     *
+     */
+    public CostItem(Category category, String description, double totalPrice, Currency currency) {
         this.id = -1;
         this.category = category;
         this.description = description;
