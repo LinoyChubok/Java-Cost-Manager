f39606d62bf5cd3f86c5af4eedd631861d817538
LinoyChubok <linoychubok@gmail.com>
2020-12-22 13:00:12 +0200

    CostItem Parameterized Constructor

--- src/CostManager/Model/CostItem.java
+++ src/CostManager/Model/CostItem.java
@@ -3,19 +3,27 @@ package CostManager.Model;
 public class CostItem {
 
     private int id;
+    private Category category;
     private String description;
     private double totalPrice;
     private Currency currency;
 
-    public CostItem(String description, double sum) {
-        //this.description = description;
-        //this.sum = sum;
-        //this.currency = currency;
-        setDescription(description);
-        //setCurrency(currency);
-        setSum(sum);
-        //assigning id with a unique value
-        //..
+    /**
+     * CostItem Parameterized Constructor that builds CostItem object.
+     *
+     @param id - Variable for holding the cost id from the database.
+     @param category - Represents the category that the cost item belong to.
+     @param description - Represents any info about the cost item.
+     @param totalPrice  - The total price of the cost.
+     @param currency - The currency that used during the purchase.
+     *
+     */
+    public CostItem(Category category, String description, double totalPrice, Currency currency, int id) {
+        this.id = -1;
+        this.category = category;
+        this.description = description;
+        this.totalPrice = totalPrice;
+        this.currency = currency;
     }
 
     public String getDescription() {
@@ -26,13 +34,13 @@ public class CostItem {
         this.description = description;
     }
 
-    public double getSum() {
-        return sum;
-    }
-
-    public void setSum(double sum) {
-        this.sum = sum;
-    }
+//    public double getSum() {
+//        return sum;
+//    }
+//
+//    public void setSum(double sum) {
+//        this.sum = sum;
+//    }
 
     //public Currency getCurrency() {
     //   return currency;
