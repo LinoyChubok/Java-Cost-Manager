15e31890db031b8c41302bac98eb32372fb2dade
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-22 14:01:53 +0200

    Add Category Constructors

--- src/CostManager/Model/Category.java
+++ src/CostManager/Model/Category.java
@@ -5,4 +5,27 @@ public class Category {
     private int id;
     private String categoryName;
 
+    /**
+     * Category Parameterized Constructor that builds Category object from Database.
+     *
+     @param id          Variable for holding the category id from the database.
+     @param categoryName    Represents the category that the cost item belong to.
+     *
+     */
+    public Category(int id, String categoryName) {
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+    /**
+     * Category Parameterized Constructor that builds Category object without id.
+     *
+     @param categoryName    Represents the category that the cost item belong to.
+     *
+     */
+    public Category(String categoryName) {
+        this.id = -1;
+        this.categoryName = categoryName;
+    }
+
 }
