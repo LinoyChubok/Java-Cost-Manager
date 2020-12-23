4dba71c69dfcefd47b4517978dbaf926a537d26a
LinoyChubok <linoychubok@gmail.com>
2020-12-23 10:35:05 +0200

    DerbyDBModel class members

--- src/CostManager/Model/DerbyDBModel.java
+++ src/CostManager/Model/DerbyDBModel.java
@@ -1,6 +1,26 @@
 package CostManager.Model;
 
+import java.sql.*;
+
 public class DerbyDBModel implements IModel {
+
+    /**
+     * DerbyDBModel will implement IModel (Each Database type has different implementations).
+     *
+     * @param PROTOCOL         Describes the communication type
+     * @param DRIVER           Describes the driver type
+     * @param connection       Holds the database connection details
+     * @param statement        Used for executing requests to the database
+     * @param rs               Holds the information that comes back from the database
+     */
+
+    private static final String PROTOCOL = "jdbc:derby:";
+    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private static ResultSet rs = null;
+
     @Override
     public void startConnection() throws CostManagerException { }
 
