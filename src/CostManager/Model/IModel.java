44280ce0e14302fd14fd8f10bcc707f495c8ec3a
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-23 12:21:02 +0200

    Update IModel

--- src/CostManager/Model/IModel.java
+++ src/CostManager/Model/IModel.java
@@ -1,8 +1,30 @@
 package CostManager.Model;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
 public interface IModel {
+
+    /**
+     * Database implementation
+     */
     public abstract void startConnection() throws CostManagerException;
     public abstract void closeConnection() throws CostManagerException;
     public abstract void createTables() throws CostManagerException;
+
+    /**
+     * Categories queries implementation
+     */
+    public abstract void addCategory(Category category) throws CostManagerException;
+    public abstract void deleteCategory(int id) throws CostManagerException;
+    public abstract ArrayList<Category> getAllCategories() throws CostManagerException;
+
+    /**
+     * CostItems queries implementation
+     */
+    public abstract void addCostItem(CostItem item) throws CostManagerException;
+    public abstract void deleteCostItem(int id) throws CostManagerException;
+    public abstract ArrayList<CostItem> getAllCostItems() throws CostManagerException;
+    public abstract ArrayList<CostItem> getAllCostItems(Date fromDate, Date toDate) throws CostManagerException;
 }
 
