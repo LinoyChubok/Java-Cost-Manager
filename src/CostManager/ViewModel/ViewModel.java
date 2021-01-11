6691a4b4850dc24f3704040298906fe20a9b113f
LinoyChubok <linoychubok@gmail.com>
2021-01-11 16:10:53 +0200

    implement functions inside ViewModel"

--- src/CostManager/ViewModel/ViewModel.java
+++ src/CostManager/ViewModel/ViewModel.java
@@ -1,12 +1,23 @@
 package CostManager.ViewModel;
 
+import CostManager.Model.CostItem;
+import CostManager.Model.CostManagerException;
 import CostManager.Model.IModel;
 import CostManager.View.IView;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ViewModel implements IViewModel{
 
     private IModel model = null;
     private IView view = null;
+    private ExecutorService pool;
+
+    public ViewModel() {
+        pool = Executors.newFixedThreadPool(10);
+    }
 
     @Override
     public void setView(IView view) {
@@ -16,5 +27,22 @@ public class ViewModel implements IViewModel{
     @Override
     public void setModel(IModel model) {
         this.model = model;
+    }
+
+    @Override
+    public void addCostItem(CostItem item) {
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    model.addCostItem(item);
+                    view.showMessage("Cost item was added successfully");
+                    ArrayList<CostItem> items = model.getAllCostItems();
+                    //view.showItems(items);
+                } catch(CostManagerException e) {
+                    view.showMessage(e.getMessage());
+                }
+            }
+        });
     }
 }
