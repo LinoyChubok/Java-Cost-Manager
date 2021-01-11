1556aa4ce03e6908d281ae91af1a3786f8e098b3
LinoyChubok <linoychubok@gmail.com>
2021-01-11 15:55:24 +0200

    Update application

--- src/CostManager/Application.java
+++ src/CostManager/Application.java
@@ -1,29 +1,3 @@
-//package CostManager;
-//
-//import CostManager.Model.*;
-//import CostManager.View.*;
-//import CostManager.ViewModel.*;
-//
-//public class Application {
-//    public static void main(String []args){
-//
-//        // Creating the application components
-//        try {
-//            IModel model = new DerbyDBModel();
-//            IView view = new View();
-//            IViewModel vm = new ViewModel();
-//
-//            // Connecting the components with each other
-//            view.setViewModel(vm);
-//            vm.setModel(model);
-//            vm.setView(view);
-//
-//        } catch (CostManagerException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//}
 package CostManager;
 
 import CostManager.Model.*;
@@ -33,9 +7,20 @@ import CostManager.ViewModel.*;
 public class Application {
     public static void main(String []args){
 
-
+        // Creating the application components
+        try {
+            IModel model = new DerbyDBModel();
             IView view = new View();
+            IViewModel vm = new ViewModel();
 
+            // Connecting the components with each other
+            view.setViewModel(vm);
+            vm.setModel(model);
+            vm.setView(view);
+
+        } catch (CostManagerException e) {
+            e.printStackTrace();
+        }
 
     }
 }
