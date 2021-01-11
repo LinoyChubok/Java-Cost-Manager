9c401e89c8b37023f390b357dadfe3bf81b1af3b
LinoyChubok <linoychubok@gmail.com>
2021-01-11 16:03:39 +0200

    Add method addCostItem to ViewModel interface

--- src/CostManager/ViewModel/IViewModel.java
+++ src/CostManager/ViewModel/IViewModel.java
@@ -1,13 +1,14 @@
 package CostManager.ViewModel;
 
-import CostManager.Model.IModel;
-import CostManager.View.IView;
-
+import CostManager.Model.*;
+import CostManager.View.*;
 
 public interface IViewModel {
 
     public abstract void setView(IView view);
     public abstract void setModel(IModel model);
+
+    public abstract void addCostItem(CostItem item);
 
 }
 
