c6f60b909300176773c12e7cd63359e9dbea3f52
LinoyChubok <linoychubok@gmail.com>
2021-01-11 15:59:08 +0200

    Add showMessaage and showItems methods to view interface

--- src/CostManager/View/IView.java
+++ src/CostManager/View/IView.java
@@ -1,12 +1,18 @@
 package CostManager.View;
 
+import CostManager.Model.CostItem;
 import CostManager.ViewModel.IViewModel;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public interface IView {
 
-    //public void displayPieChart(Map map);
     public abstract void setViewModel(IViewModel vm);
+
+    public abstract void showMessage(String text);
+    public abstract void showItems(ArrayList<CostItem> items);
+
+    //public abstract void showPieChart(Map map);
 
 }
